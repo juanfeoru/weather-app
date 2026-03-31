3a8204a55afda83b3dadfb8375472495085b29c5
juanfeoru <felipeortega383@gmail.com>
2026-03-31 12:38:15 -0500

    feat: add reusable Input component with search icon support

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -4,26 +4,18 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   showIcon?: boolean;
 }
 
-export default function Input({
-  showIcon = true,
-  className,
-  ...props
-}: InputProps) {
+export default function Input({ className, ...props }: InputProps) {
   return (
     <div className='relative w-full'>
-      {showIcon && (
-        <img
-          src={iconSearch}
-          className='absolute left-4 top-1/2 -translate-y-1/2 pointer-events-none'
-          alt=''
-        />
-      )}
+      <img
+        src={iconSearch}
+        className='absolute left-4 top-1/2 -translate-y-1/2 pointer-events-none'
+        alt=''
+      />
 
       <input
         type='text'
-        className={`w-full px-4 py-3 rounded-lg bg-neutral-800 text-white outline-none focus:ring-2 focus:ring-blue-500 ${
-          showIcon ? 'pl-12' : ''
-        } ${className}`}
+        className={`w-full px-4 py-3 rounded-lg bg-neutral-800 text-white outline-none focus:ring-2 focus:ring-blue-500 pl-12 ${className}`}
         {...props}
       />
     </div>
